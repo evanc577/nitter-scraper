43361aba022a892ede662a822afd9240e480669a
Evan Chang <evanc577@gmail.com>
2023-04-24 02:55:54 -0400

    Use once_cell for static init

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,14 @@ use std::collections::VecDeque;
 
 use error::NitterError;
 use futures_util::Stream;
+use once_cell::sync::Lazy;
 use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
 use regex::Regex;
 use reqwest::header::COOKIE;
 use reqwest::Client;
 use scraper::{Html, Selector};
 use time::format_description::well_known::Rfc3339;
+use time::format_description::FormatItem;
 use time::macros::format_description;
 use time::PrimitiveDateTime;
 use tweet::Tweet;
@@ -139,26 +141,33 @@ impl<'a> NitterScraper<'a> {
 }
 
 fn parse_nitter_html(html: String) -> Result<(Vec<Tweet>, String), NitterError> {
-    let document = Html::parse_document(&html);
-    let tweet_selector = Selector::parse(".timeline-item:not(.show-more)").unwrap();
-    let tweet_link_selector = Selector::parse("a.tweet-link").unwrap();
-    let tweet_link_re = Regex::new(r"^/(?P<screen_name>\w+)/status/(?P<id>\d+)").unwrap();
-    let tweet_body_selector = Selector::parse(".tweet-content").unwrap();
-    let images_selector = Selector::parse(".attachment.image a.still-image").unwrap();
-    let image_id_re = Regex::new(r"^/pic/\w+/media%2F(?P<url>\w+\.\w+)$").unwrap();
-    let tweet_date_selector = Selector::parse("span.tweet-date a").unwrap();
-    let time_format_description = format_description!(
+    static TWEET_SELECTOR: Lazy<Selector> =
+        Lazy::new(|| Selector::parse(".timeline-item:not(.show-more)").unwrap());
+    static TWEET_LINK_SELECTOR: Lazy<Selector> =
+        Lazy::new(|| Selector::parse("a.tweet-link").unwrap());
+    static TWEET_LINK_RE: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"^/(?P<screen_name>\w+)/status/(?P<id>\d+)").unwrap());
+    static TWEET_BODY_SELECTOR: Lazy<Selector> =
+        Lazy::new(|| Selector::parse(".tweet-content").unwrap());
+    static IMAGES_SELECTOR: Lazy<Selector> =
+        Lazy::new(|| Selector::parse(".attachment.image a.still-image").unwrap());
+    static IMAGE_ID_RE: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"^/pic/\w+/media%2F(?P<url>\w+\.\w+)$").unwrap());
+    static TWEET_DATE_SELECTOR: Lazy<Selector> =
+        Lazy::new(|| Selector::parse("span.tweet-date a").unwrap());
+    static TIME_FORMAT_DESCRIPTION: &[FormatItem<'_>] = format_description!(
         "[month repr:short] [day padding:none], [year] · [hour repr:12 padding:none]:[minute] [period] UTC"
     );
 
     let mut tweets = vec![];
 
-    for element in document.select(&tweet_selector) {
+    let document = Html::parse_document(&html);
+    for element in document.select(&TWEET_SELECTOR) {
         // Parse tweet author screen_name and tweet id
         let (screen_name, id_str) = {
-            let tweet_link_element = element.select(&tweet_link_selector).next().unwrap();
+            let tweet_link_element = element.select(&TWEET_LINK_SELECTOR).next().unwrap();
             let tweet_link = tweet_link_element.value().attr("href").unwrap();
-            let caps = tweet_link_re.captures(tweet_link).unwrap();
+            let caps = TWEET_LINK_RE.captures(tweet_link).unwrap();
             let screen_name = caps.name("screen_name").unwrap().as_str();
             let id_str = caps.name("id").unwrap().as_str();
             (screen_name, id_str)
@@ -167,7 +176,7 @@ fn parse_nitter_html(html: String) -> Result<(Vec<Tweet>, String), NitterError> 
 
         // Parse tweet body
         let full_text: String = element
-            .select(&tweet_body_selector)
+            .select(&TWEET_BODY_SELECTOR)
             .next()
             .unwrap()
             .text()
@@ -176,11 +185,11 @@ fn parse_nitter_html(html: String) -> Result<(Vec<Tweet>, String), NitterError> 
 
         // Parse images
         let images: Vec<_> = element
-            .select(&images_selector)
+            .select(&IMAGES_SELECTOR)
             .into_iter()
             .filter_map(|e| {
                 let link = e.value().attr("href").unwrap();
-                match image_id_re.captures(link) {
+                match IMAGE_ID_RE.captures(link) {
                     Some(caps) => Some(format!(
                         "https://pbs.twimg.com/media/{}",
                         caps.name("url").unwrap().as_str()
@@ -192,9 +201,9 @@ fn parse_nitter_html(html: String) -> Result<(Vec<Tweet>, String), NitterError> 
 
         // Parse date
         let created_at = {
-            let tweet_date_element = element.select(&tweet_date_selector).next().unwrap();
+            let tweet_date_element = element.select(&TWEET_DATE_SELECTOR).next().unwrap();
             let time_str = tweet_date_element.value().attr("title").unwrap();
-            let time = PrimitiveDateTime::parse(time_str, time_format_description).unwrap();
+            let time = PrimitiveDateTime::parse(time_str, TIME_FORMAT_DESCRIPTION).unwrap();
             let time = time.assume_utc();
             time.format(&Rfc3339).unwrap()
         };
@@ -212,9 +221,9 @@ fn parse_nitter_html(html: String) -> Result<(Vec<Tweet>, String), NitterError> 
     }
 
     // Parse cursor
-    let cursor_selector = Selector::parse(".show-more a").unwrap();
+    static CURSOR_SELECTOR: Lazy<Selector> = Lazy::new(|| Selector::parse(".show-more a").unwrap());
     let cursor = document
-        .select(&cursor_selector)
+        .select(&CURSOR_SELECTOR)
         .last()
         .unwrap()
         .value()
